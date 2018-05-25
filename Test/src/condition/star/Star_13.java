ca3ff9c6318aa87cf19367f2344db30cbcc7ac2f
user2 <user2@KH_B>
2018-05-25 15:36:35 +0900

    5.25 별찍기13 수정

--- Test/src/condition/star/Star_13.java
+++ Test/src/condition/star/Star_13.java
@@ -15,9 +15,9 @@ public class Star_13 {
 		}
 		
 		
-	for(int n=0 ; n <5 ; n++) {
+	for(int n=0 ; n <4 ; n++) {
 		
-		for(int m=0 ; m<n+1; m++) {
+		for(int m=0 ; m<n+2; m++) {
 	
 			System.out.print("*");		
 	
