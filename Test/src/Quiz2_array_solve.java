0914e47beb6e509b204e5887d6fd8d58686c5e04
hidjo <hidjo@DESKTOP-5SIJPMQ>
2018-05-29 15:15:39 +0900

    5.29 study

--- Test/src/Quiz2_array_solve.java
+++ Test/src/Quiz2_array_solve.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Quiz2_array_solve {
@@ -7,6 +8,12 @@ public class Quiz2_array_solve {
 		
 		// int arr[] 선언 //
 		int arr[] = {10, 17, 3, 9, 27, 10, 8, 9, 13, 21};
+		
+		System.out.println("-------1번---------");
+		for(int i=0; i<arr.length; i++) {
+		System.out.print(arr[i] + " ");
+		}
+		System.out.println();
 		
 		
 		
@@ -19,6 +26,13 @@ public class Quiz2_array_solve {
 		
 			invert_arr[9-i]= arr[i];
 		}
+		
+		System.out.println("------------2번-------");
+		for(int i=0 ; i<invert_arr.length ; i++) {
+			System.out.println(invert_arr[i] + " ");
+			
+		}
+		System.out.println();
 		
 		
 		
@@ -41,66 +55,77 @@ public class Quiz2_array_solve {
 			
 		}
 		
+		System.out.println("----------3번--------");
+		System.out.println("total : "+total);
+		
 		// 높은 숫자대로 rank_arr배열에 입력 // ??
 		
-		int[] rank_arr = new int[10];
+		int[] rank_arr = new int[arr.length];
 		int rank=1;
 		
 		for(int i=0 ; i < arr.length ; i++ ) {
 			for(int q=0 ; q <arr.length ; q++) {
 				if(i!=q) {
 					if(arr[i]<arr[q]) {
-						rank ++;
-						rank_arr[i]=rank;
+						rank ++;					
 					}
 				}
-			}
+			}			
+			rank_arr[i]=rank;
+			rank=1;
 		}
+	     System.out.println("----------4번-----");   
+	     
+	     
+	        for(int i=0; i<arr.length ; i++) {
+	            System.out.print(arr[i]+" ");
+	            }
+	        System.out.println();
+	        for(int i=0; i<rank_arr.length ; i++) {
+	        System.out.print(rank_arr[i]+" ");
+	        
+	        }
+	        
+	        System.out.println();
+		
 		
 		// 정렬까지 했다면 //
 		
-		int num = 0;
-		int[] over_arr = new int[2];
-		int[] ret_arr = new int[6];
+        int c=0;
+        int d=0;
+        int e=0;
+        int over_arr[] = new int[arr.length];
+        int ret_arr[] = new int[arr.length];
 		
-		for(int i=0 ; i< arr.length ; i++) {
-			
-			for(int q=0 ; q <arr.length ; q++) {
-				
-				if(i!=q) {
-					
-					if(arr[i] == arr[q]) {
-						
-						
-						if(num==0){
-							
-					
-						
-						
-					}
-				}
-				
-				
-			}
-			
-			
-			
-		}
-				
-		
-		
-		
-	
-		
-		
-		}
-		
-		
-		
-		
-		
-		
+        for(int i=1; i<=arr.length ; i++) {
+          for(int q=0 ; q<arr.length ; q++) {
+        	  if(i==rank_arr[q]) {
+        		  c++;
+           	  }
+          }
+          if(c==2) {
+        	  over_arr[d] = arr[i-1];
+        	  d++;
+        	  c=0;
+          }else {
+        	  ret_arr[e] = arr[i-1];
+        	  e++;
+        	  c=0;
+          }
+        }
+  
+        
+        System.out.println("----------5번-----");   
+     
+        for(int i=0; i<=d ; i++) {
+        System.out.print(over_arr[i]+" ");
+        }
+        System.out.println();
+        for(int i=0; i<=e ; i++) {
+            System.out.print(ret_arr[i]+" ");
+            }
 	}
-	
+}
+
 	
 
